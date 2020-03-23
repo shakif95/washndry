1c833480ddd428825768f561573f771752bcfe59
Shakif95 <shakif.malek@gmail.com>
2020-03-23 18:26:34 +0600

    [added] appointment card in the component

--- src/components/dashboard/Appointments/Appointments.tsx
+++ src/components/dashboard/Appointments/Appointments.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
 import { Dashboard } from '../Dashboard';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 
 import './Appointments.css';
 
 export const Appointments: React.FC<any> = props => {
   return (
     <Dashboard>
-      <Container fluid>
-        <Row>
-          <Col md={12}>
-            <Card>
-              <Card.Header>
-                <Card.Title>
-                  MY APPOINTMENTS
+      <Row>
+        <Col md={12}>
+          <Card>
+            <Card.Header>
+              <Card.Title>
+                MY APPOINTMENTS
                 </Card.Title>
-                <Card.Subtitle className="card-subtitles">
-                  This view is showing your scheduled appointments
+              <Card.Subtitle className="card-subtitles">
+                This view is showing your scheduled appointments
                 </Card.Subtitle>
-              </Card.Header>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+            </Card.Header>
+            <Card.Body>
+              <Row>
+                <Col xs={12} md={6} lg={3}>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Dashboard>
   )
 }

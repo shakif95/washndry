92e34335c9e4ec53c10f0e2ccbba53d11655d5b7
Shakif Malek <shakif.malek@gmail.com>
2020-03-28 14:33:15 +0600

    [Added] new cards for each appointment

--- src/components/dashboard/Appointments/Appointments.tsx
+++ src/components/dashboard/Appointments/Appointments.tsx
@@ -21,9 +21,116 @@ export const Appointments: React.FC<any> = props => {
             <Card.Body>
               <Row>
                 <Col xs={12} md={6} lg={3}>
-                  <Button>
+                  <Button variant="success">
                     Book Now
                   </Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} md={6} lg={4}>
+                  <Card className="appointment-card">
+                    <Card.Header>
+                      <Card.Title>
+                        21st August, 2020
+                        <p className="day">Friday</p>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        2.00PM - 2.45PM
+                      </Card.Subtitle>
+                    </Card.Header>
+                    <Card.Body className="card-body-spacing">
+                        <Button>
+                          Change
+                        </Button>
+                        <Button variant="danger">
+                          Cancel
+                        </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                  <Card className="appointment-card">
+                    <Card.Header>
+                      <Card.Title>
+                        28th August, 2020
+                        <p className="day">Friday</p>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        2.00PM - 2.45PM
+                      </Card.Subtitle>
+                    </Card.Header>
+                    <Card.Body className="card-body-spacing">
+                        <Button>
+                          Change
+                        </Button>
+                        <Button variant="danger">
+                          Cancel
+                        </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                  <Card className="appointment-card">
+                    <Card.Header>
+                      <Card.Title>
+                        4th September, 2020
+                        <p className="day">Friday</p>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        2.00PM - 2.45PM
+                      </Card.Subtitle>
+                    </Card.Header>
+                    <Card.Body className="card-body-spacing">
+                        <Button>
+                          Change
+                        </Button>
+                        <Button variant="danger">
+                          Cancel
+                        </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                  <Card className="appointment-card">
+                    <Card.Header>
+                      <Card.Title>
+                        11th September, 2020
+                        <p className="day">Friday</p>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        2.00PM - 2.45PM
+                      </Card.Subtitle>
+                    </Card.Header>
+                    <Card.Body className="card-body-spacing">
+                        <Button>
+                          Change
+                        </Button>
+                        <Button variant="danger">
+                          Cancel
+                        </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                  <Card className="appointment-card">
+                    <Card.Header>
+                      <Card.Title>
+                        18th September, 2020
+                        <p className="day">Friday</p>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        2.00PM - 2.45PM
+                      </Card.Subtitle>
+                    </Card.Header>
+                    <Card.Body className="card-body-spacing">
+                        <Button>
+                          Change
+                        </Button>
+                        <Button variant="danger">
+                          Cancel
+                        </Button>
+                    </Card.Body>
+                  </Card>
                 </Col>
               </Row>
             </Card.Body>
